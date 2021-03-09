ec0adcb6c001c2992aabc7dd4cd4d6a5e608c3b0
Elly <ellychau10@yahoo.com>
2021-03-08 18:46:47 -0800

    Updated take_turn method and show_sequence

--- skeleton/lib/simon.rb
+++ skeleton/lib/simon.rb
@@ -10,31 +10,49 @@ class Simon
   end
 
   def play
+    until @game_over
 
+    end
   end
 
   def take_turn
-
+    show_sequence
+    i=0
+    p "please enter the sequence"
+    until i == @sequence_length
+      user_seq = require_sequence
+      if @seq[i] == user_seq
+        round_success_message
+      else
+        game_over_message
+        return false
+      end
+      i += 1
+    end
+    @sequence_length += 1
   end
 
   def show_sequence
-
+    @seq.each do |color| 
+      p color
+      sleep(1)
+    end
   end
 
   def require_sequence
-
+    gets.chomp
   end
 
   def add_random_color
-
+    @seq << COLORS.sample
   end
 
   def round_success_message
-
+    p "success!"
   end
 
   def game_over_message
-
+    p "sorry you lost!"
   end
 
   def reset_game
